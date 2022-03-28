e123b9db5ca56d2371a89b5c8ad2f6987e6b1f04
Amanda L Martin <hythloda@gmail.com>
2022-03-28 10:43:49 -0600

    Update 4-combine.py

--- data/notebooks/4-combine.py
+++ data/notebooks/4-combine.py
@@ -21,6 +21,25 @@ agg_list_coins = as_list([
     agg.AggAvg("Average_open = Open"),
 ])
 
+agg_list_combined = as_list([
+    agg.AggCount("Count_tweet"),
+    agg.AggAvg("Average_negative = Negative"),
+    agg.AggAvg("Average_neutral = Neutral"),
+    agg.AggAvg("Average_positive = Positive"),
+    agg.AggAvg("Average_compound = Compound"),
+    agg.AggWAvg("Retweet_count", "Weight_negative = Negative"),
+    agg.AggWAvg("Retweet_count","Weight_neutral = Neutral"),
+    agg.AggWAvg("Retweet_count","Weight_positive = Positive"),
+    agg.AggWAvg("Retweet_count","Weight_compound = Compound"),
+    agg.AggCount("Count_coin"),
+    agg.AggAvg("Average_close = Close"),
+    agg.AggAvg("Average_high = High"),
+    agg.AggAvg("Average_low = Low"),
+    agg.AggAvg("Average_open = Open"),
+])
+
+
+
 nanosBin = expressionToNanos("T10M")
 
 agged_tweet_data = sia_data.update("Time_bin = upperBin(DateTime,nanosBin)")
@@ -30,3 +49,6 @@ agged_coin_data = coin_data.update("Time_bin = upperBin(DateTime,nanosBin)")
 combined_coins = agged_coin_data.aggBy(agg_list_coins,"Time_bin").sort("Time_bin")
 
 combined_data = combined_tweets.aj(combined_coins,"Time_bin").update("Average_negative=Average_negative*-100", "Weight_compound=Weight_compound*100", "Average_positive =100* Average_positive" )
+
+
+
