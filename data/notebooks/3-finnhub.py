646be4b303a93eb4104ddfe00edd386af8101d08
Amanda L Martin <hythloda@gmail.com>
2022-03-28 07:38:05 -0600

    Update and rename finnhub.py to 3-finnhub.py

--- data/notebooks/3-finnhub.py
+++ data/notebooks/3-finnhub.py
@@ -1,16 +1,3 @@
-import finnhub
-import pandas as pd
-import re
-from datetime import datetime
-import time
-
-from deephaven.DateTimeUtils import currentTime, secondsToTime, convertDateTime, millis
-
-from deephaven import DynamicTableWriter
-import deephaven.Types as dht
-import threading
-
-
 def pull_coins():
     exchanges = finnhub_client.crypto_exchanges()
     temp_ids = []
@@ -27,22 +14,19 @@ def pull_coins():
 ids = pull_coins()
 
 def thread_func():
-        for i in range(50):
-            for id in ids:
-                data = finnhub_client.crypto_candles(id, 15, int(millis(minus(currentTime(),convertPeriod("T"+str(int((24*time_history)))+"H")))/1000), int(datetime.timestamp(datetime.now())))
-                coin = id
-                if data['s'] =='ok' and len(data['s'])>0:
-                    c = (data['c'])
-                    h = (data['h'])
-                    l = (data['l'])
-                    o = (data['o'])
-                    t = (data['t'])
-                    v = (data['v'])
-                    if c != None:
-                        for i in range(len(c)): 
-                            tableWriter.logRow(coin, float(c[i]), float(h[i]), float(l[i]), float(o[i]), secondsToTime(t[i]), float(v[i]))
-            time.sleep(1)
-
+    for id in ids:
+        data = finnhub_client.crypto_candles(id, 15, int(millis(minus(currentTime(),convertPeriod("T"+str(int((24*time_history)))+"H")))/1000), int(datetime.timestamp(datetime.now())))
+        coin = id
+        if data['s'] =='ok' and len(data['s'])>0:
+            c = (data['c'])
+            h = (data['h'])
+            l = (data['l'])
+            o = (data['o'])
+            t = (data['t'])
+            v = (data['v'])
+            if c != None:
+                for i in range(len(c)): 
+                    tableWriter.logRowPermissive(coin, float(c[i]), float(h[i]), float(l[i]), float(o[i]), secondsToTime(t[i]), float(v[i]))
 
 
 tableWriter = DynamicTableWriter(
